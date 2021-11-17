6b520e74850a9b88d19efa5e09bd4772caae6fe9
yasu2450 <hiro0130.tech@gmail.com>
2021-11-17 11:49:13 +0900

    add: ヘッダーの作成#3

---
+++ front/components/layouts/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app-bar fixed app color="blue-grey lighten-1">
+    <v-app-bar-title class="white--text">
+      ホーム
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <the-header />
     <v-main>
       <v-container>
         <Nuxt />
@@ -7,3 +8,12 @@
     </v-main>
   </v-app>
 </template>
+
+<script>
+import TheHeader from '~/components/layouts/TheHeader.vue'
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
