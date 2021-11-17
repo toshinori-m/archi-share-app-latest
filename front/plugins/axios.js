b3459bf2c16987438dfd3203677471d4288bc2a4
yasu2450 <hiro0130.tech@gmail.com>
2021-11-17 13:08:51 +0900

    add: Axiosの設定をplugins内に作成#19

---
+++ front/plugins/axios.js
@@ -0,0 +1,21 @@
+export default function({ $axios }) {
+  $axios.onRequest((config) => {
+    config.headers.client = window.localStorage.getItem('client')
+    config.headers['access-token'] = window.localStorage.getItem('access-token')
+    config.headers.uid = window.localStorage.getItem('uid')
+    config.headers['token-type'] = window.localStorage.getItem('token-type')
+    config.headers.expiry = window.localStorage.getItem('expiry')
+    config.headers['content-type'] = 'application/json'
+  })
+
+  $axios.onResponse((response) => {
+    $axios.setHeader('Access-Control-Allow-Origin', '*')
+    if (response.headers.client) {
+      localStorage.setItem('access-token', response.headers['access-token'])
+      localStorage.setItem('client', response.headers.client)
+      localStorage.setItem('uid', response.headers.uid)
+      localStorage.setItem('token-type', response.headers['token-type'])
+      localStorage.setItem('expiry', response.headers.expiry)
+    }
+  })
+}
