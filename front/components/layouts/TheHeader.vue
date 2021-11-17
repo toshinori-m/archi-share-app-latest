8313e1ffba3fdfe139ddc1f9a41cf6e3f800213c
yasu2450 <hiro0130.tech@gmail.com>
2021-11-17 14:31:29 +0900

    ユーザーログアウト機能の作成#27

--- front/components/layouts/TheHeader.vue
+++ front/components/layouts/TheHeader.vue
@@ -4,18 +4,26 @@
       ホーム
     </v-app-bar-title>
     <v-spacer />
-    <the-sign-up />
-    <the-sign-in />
+    <template v-if="!login">
+      <the-sign-up />
+      <the-sign-in />
+    </template>
   </v-app-bar>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import TheSignUp from '~/components/layouts/TheSignUp.vue'
 import TheSignIn from '~/components/layouts/TheSignIn.vue'
 export default {
   components: {
     TheSignUp,
     TheSignIn
+  },
+  computed: {
+    ...mapGetters('authentication', [
+      'login'
+    ])
   }
 }
 </script>
---
+++ front/components/layouts/TheSignOut.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-tooltip bottom>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          icon
+          large
+          v-bind="attrs"
+          v-on="on"
+          @click="dialog = true"
+        >
+          <v-icon color="white">mdi-close</v-icon>
+        </v-btn>
+      </template>
+      <span class="text-caption">ログアウト</span>
+    </v-tooltip>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="300px"
+      max-height="400px"
+    >
+      <v-card>
+        <v-card-title class="justify-center">
+          <span>ログアウトの確認</span>
+        </v-card-title>
+        <v-card-text class="black--text text-center">
+          <p>ログアウトしてもよろしいですか？</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn block color="light-blue lighten-2" @click="signOutAction">はい</v-btn>
+        </v-card-actions>
+        <v-card-actions class="pb-4">
+          <v-btn block @click="dialog = false">いいえ</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  methods: {
+    ...mapActions('authentication', [
+      'userSignOut'
+    ]),
+    signOutAction() {
+      this.$router.push('/')
+      this.dialog = false
+      this.userSignOut()
+    }
+  }
+}
+</script>
