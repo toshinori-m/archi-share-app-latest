068d7f71ef462164082fa03b5d079e64046649a5
yasu2450 <hiro0130.tech@gmail.com>
2021-11-18 14:15:08 +0900

    add: スナックバー機能の作成#57

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -3,6 +3,7 @@
     <the-navigation-drawer />
     <the-header />
     <v-main>
+      <the-snackbar />
       <v-container>
         <Nuxt />
       </v-container>
@@ -13,10 +14,12 @@
 <script>
 import TheNavigationDrawer from '~/components/layouts/TheNavigationDrawer.vue'
 import TheHeader from '~/components/layouts/TheHeader.vue'
+import TheSnackbar from '~/components/layouts/TheSnackbar.vue'
 export default {
   components: {
     TheHeader,
-    TheNavigationDrawer
+    TheNavigationDrawer,
+    TheSnackbar
   }
 }
 </script>
