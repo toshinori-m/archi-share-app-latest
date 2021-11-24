60ab97335310fb9f945f9750072a60c036fe5682
yasu2450 <hiro0130.tech@gmail.com>
2021-11-24 20:45:17 +0900

    fix: フォロワーに自分が表示されるエラーの解消#103

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy, inverse_of: :user
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   def follow(other_user)
