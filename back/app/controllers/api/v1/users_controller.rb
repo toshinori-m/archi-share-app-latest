3a39a68c67312b1034a8368c9ecc40336e42c6db
yasu2450 <hiro0130.tech@gmail.com>
2021-12-07 00:31:19 +0900

    fix: Rubocopの指摘箇所の修正#131

--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -18,20 +18,8 @@ module Api
         render json: @user.as_json(
           only: %i[id name email introduction image],
           include: [
-            {
-              posts: {
-                include: [:like_users],
-                only: %i[id title content image]
-              }
-            },
-            {
-              postlike: {
-                include: [
-                  { user: { only: %i[id name image] } },
-                  :like_users
-                ]
-              }
-            },
+            { posts: { include: [:like_users], only: %i[id title content image] } },
+            { postlike: { include: [{ user: { only: %i[id name image] } }, :like_users] } },
             :followings,
             :followers
           ]
