7a0fe817d4607ecb9611d4ec569ed78ed071f5de
yasu2450 <hiro0130.tech@gmail.com>
2021-12-07 19:57:42 +0900

    fix: ユーザーページの投稿がない場合の表示を追加#139

--- front/components/like/LikePostList.vue
+++ front/components/like/LikePostList.vue
@@ -1,53 +1,62 @@
 <template>
   <div>
-    <v-card
-      v-for="post in posts"
-      :key="post.id"
-      tile
-      outlined
-      @click="postClick(post)"
-    >
-      <v-card-actions>
-        <div
-          class="d-inline-block"
-          @click.stop="userClick(post)"
-        >
-          <v-avatar size="50" class="mr-2">
-            <v-img v-if="post.user.image.url" :src="post.user.image.url" />
-            <v-img v-else :src="icon" />
-          </v-avatar>
-          <span>
-            {{ post.user.name }}
-          </span>
+    <template v-if="user.postlike[0]">
+      <v-card
+        v-for="post in user.postlike"
+        :key="post.id"
+        tile
+        outlined
+        @click="postClick(post)"
+      >
+        <v-card-actions>
+          <div
+            class="d-inline-block"
+            @click.stop="userClick(post)"
+          >
+            <v-avatar size="50" class="mr-2">
+              <v-img v-if="post.user.image.url" :src="post.user.image.url" />
+              <v-img v-else :src="icon" />
+            </v-avatar>
+            <span>
+              {{ post.user.name }}
+            </span>
+          </div>
+        </v-card-actions>
+        <div class="pa-4">
+          <v-row algin="center">
+            <v-col cols="6" align-self="center">
+              <v-img
+                :src="post.image.url"
+                contain
+                max-height="250"
+              />
+            </v-col>
+            <v-col cols="6">
+              <v-responsive max-height="210">
+                <v-card-title class="justify-center">
+                  <span class="text-truncate">{{ post.title }}</span>
+                </v-card-title>
+                <v-card-text>
+                  <p class="overflow-y-hidden">
+                    {{ post | filteredContent }}
+                  </p>
+                </v-card-text>
+                <div>
+                  <like-button :post="post" />
+                </div>
+              </v-responsive>
+            </v-col>
+          </v-row>
         </div>
-      </v-card-actions>
-      <div class="pa-4">
-        <v-row algin="center">
-          <v-col cols="6" align-self="center">
-            <v-img
-              :src="post.image.url"
-              contain
-              max-height="250"
-            />
-          </v-col>
-          <v-col cols="6">
-            <v-responsive max-height="210">
-              <v-card-title class="justify-center">
-                <span class="text-truncate">{{ post.title }}</span>
-              </v-card-title>
-              <v-card-text>
-                <p class="overflow-y-hidden">
-                  {{ post | filteredContent }}
-                </p>
-              </v-card-text>
-              <div>
-                <like-button :post="post" />
-              </div>
-            </v-responsive>
-          </v-col>
-        </v-row>
-      </div>
-    </v-card>
+      </v-card>
+    </template>
+    <template>
+      <v-card tile outlined>
+        <v-card-text>
+          <p class="text-h5 text-center mb-0">{{ user.name }}がいいねした投稿はありません</p>
+        </v-card-text>
+      </v-card>
+    </template>
   </div>
 </template>
 
@@ -63,7 +72,7 @@ export default {
     }
   },
   props: {
-    posts: {
+    user: {
       type: Array,
       required: true
     }
