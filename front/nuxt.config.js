8355d2b0715d6671be26625d157259906f752ec6
yasu2450 <hiro0130.tech@gmail.com>
2021-12-09 16:32:07 +0900

    add: Day.jsの導入#151

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/axios.js' },
+    { src: '~/plugins/day.js' },
     { src: '~/plugins/persistedState.client.js' }
   ],
 
