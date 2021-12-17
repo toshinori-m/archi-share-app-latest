f34d456959c250b5787923c7b1e74497cd6150fa
yasu2450 <hiro0130.tech@gmail.com>
2021-12-17 13:24:30 +0900

    fix: nuxt.config.jsとplugins内のMap関連のファイルの修正#181

--- front/plugins/vue2-google-maps.js
+++ front/plugins/vue2-google-maps.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
 
-export default ({ $config }) => {
+export default function({ $config }) {
   Vue.use(VueGoogleMaps, {
     load: {
-      key: `${$config.API_KEY}`,
+      key: $config.API_KEY,
       libraries: 'places'
     }
   })
 }
-
