c10591d5c6840c7fd316b37bc45514906f8bc1fe
yasu2450 <hiro0130.tech@gmail.com>
2021-12-22 16:41:34 +0900

    update: ユーザーフォローリストのレイアウトの改良#225

--- front/components/user/UserFollowList.vue
+++ front/components/user/UserFollowList.vue
@@ -2,13 +2,21 @@
   <v-card>
     <v-card-actions>
       <v-spacer />
-      <v-btn icon large @click="closeDialog">
+      <v-btn
+        icon
+        large
+        @click="closeDialog"
+      >
         <v-icon>
           mdi-close
         </v-icon>
       </v-btn>
     </v-card-actions>
-    <v-tabs v-model="tab" grow>
+    <v-tabs
+      v-model="tab"
+      grow
+      color="primary"
+    >
       <v-tab v-for="item in items" :key="item">
         {{ item }}
       </v-tab>
@@ -22,12 +30,18 @@
               :key="following.id"
               @click="userClick(following)"
             >
-              <v-list-item-avatar size="60">
+              <v-list-item-avatar :size="avatarSize">
                 <v-img v-if="following.image.url" :src="following.image.url" />
                 <v-img v-else :src="icon" />
               </v-list-item-avatar>
               <v-list-item-content>
-                <span>{{ following.name }}</span>
+                <span
+                  class="text-xs-caption
+                  text-sm-subtitle1
+                  text-truncate"
+                >
+                  {{ following.name }}
+                </span>
               </v-list-item-content>
               <v-list-item-action>
                 <user-follow-button :user="following" />
@@ -47,12 +61,18 @@
               :key="follower.id"
               @click="userClick(follower)"
             >
-              <v-list-item-avatar size="60">
+              <v-list-item-avatar :size="avatarSize">
                 <v-img v-if="follower.image.url" :src="follower.image.url" />
                 <v-img v-else :src="icon" />
               </v-list-item-avatar>
               <v-list-item-content>
-                <span>{{ follower.name }}</span>
+                <span
+                  class="text-xs-caption
+                  text-sm-subtitle1
+                  text-truncate"
+                >
+                  {{ follower.name }}
+                </span>
               </v-list-item-content>
               <v-list-item-action>
                 <user-follow-button :user="follower" />
@@ -74,6 +94,11 @@ import UserFollowButton from '~/components/user/UserFollowButton.vue'
 export default {
   components: {
     UserFollowButton
+  },
+  filters: {
+    filteredName(name) {
+      return name.length > 10 ? name.slice(0, 10) + '...' : name
+    }
   },
   props: {
     followings: {
@@ -101,7 +126,14 @@ export default {
   computed: {
     ...mapGetters('user', [
       'user'
-    ])
+    ]),
+    avatarSize() {
+      if (this.$vuetify.breakpoint.smAndUp) {
+        return 60
+      } else {
+        return 45
+      }
+    }
   },
   mounted() {
     if (this.tabs) {
