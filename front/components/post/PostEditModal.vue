fb802a9fb0eaf5b3d5771a87c3b5042895d49151
yasu2450 <hiro0130.tech@gmail.com>
2021-12-23 14:15:59 +0900

    add: 投稿編集画面に建築物の選択フォームを追加#243

--- front/components/post/PostEditModal.vue
+++ front/components/post/PostEditModal.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-card-actions class="pb-0">
+    <v-card-actions class="pa-0">
       <v-spacer />
       <v-btn
         icon
@@ -12,15 +12,29 @@
         </v-icon>
       </v-btn>
     </v-card-actions>
-    <v-card-title class="justify-center pa-0">
-      <span class="headline">投稿編集</span>
-    </v-card-title>
+    <v-toolbar dense flat color="tertiary" class="mb-4">
+      <v-toolbar-title class="primary--text mx-auto">
+        投稿編集
+      </v-toolbar-title>
+    </v-toolbar>
     <v-card-text>
       <v-form ref="form">
+        <v-autocomplete
+          v-model="architecture"
+          :items="architectures"
+          item-text="name"
+          item-value="id"
+          no-data-text="該当する建築物はありません"
+          placeholder="紐付けしたい建築物を選択"
+          clearable
+          color="secondary"
+          prepend-icon="mdi-home"
+        />
         <v-file-input
           v-model="sendImage"
           accept="image/png, image/jpeg, image/bmp"
-          label="画像を変更する場合は選択してください"
+          label="画像を変更する場合は選択"
+          color="secondary"
           prepend-icon="mdi-image"
           @change="setImage"
         />
@@ -30,6 +44,7 @@
           :rules="titleRules"
           validate-on-blur
           counter="30"
+          color="secondary"
           prepend-icon="mdi-lead-pencil"
         />
         <v-textarea
@@ -38,11 +53,12 @@
           :rules="contentRules"
           validate-on-blur
           counter="200"
+          color="secondary"
           prepend-icon="mdi-note-text"
         />
       </v-form>
     </v-card-text>
-    <v-card-actions class="justify-center pa-5">
+    <v-card-actions class="justify-center pa-5 pt-0">
       <v-btn
         :disabled="imageCheck"
         color="green"
@@ -56,7 +72,7 @@
       </v-dialog>
       <v-btn
         width="125"
-        color="light-blue lighten-2"
+        color="secondary"
         @click="postEditAction"
       >
         編集
@@ -78,6 +94,8 @@ export default {
       content: '',
       image: null,
       sendImage: null,
+      architecture: null,
+      architectures: [],
       titleRules: [
         v => !!v || '投稿名を入力してください',
         v => (v && v.length <= 30) || '30文字以内で入力してください'
@@ -105,6 +123,17 @@ export default {
   mounted() {
     this.title = this.post.title
     this.content = this.post.content
+    if (this.post.architecture) {
+      this.architecture = this.post.architecture
+    }
+    this.$axios
+      .get('/api/v1/architectures')
+      .then((res) => {
+        this.architectures = res.data
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   },
   methods: {
     ...mapActions({
@@ -133,6 +162,11 @@ export default {
       const formData = new FormData()
       formData.append('title', this.title)
       formData.append('content', this.content)
+      if (this.architecture !== null) {
+        formData.append('architecture_id', this.architecture)
+      } else {
+        formData.append('architecture_id', '')
+      }
       if (this.sendImage) {
         formData.append('image', this.sendImage)
       }
