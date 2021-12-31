b9122cd5f41939c33779fbcd5634824dbe50a8d1
yasu2450 <hiro0130.tech@gmail.com>
2021-12-31 16:31:26 +0900

    fix: 建築物の位置情報の参照データを変更する#259

--- front/components/architecture/ArchitecturePostMap.vue
+++ front/components/architecture/ArchitecturePostMap.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="px-2">
-    <template v-if="loading">
-      <GmapMap
-        :center="{ lat: lat, lng: lng }"
-        :zoom="17"
-        :options="options"
-        map-type-id="roadmap"
-        style="height: 300px"
-      >
-        <GmapMarker :position="{ lat: lat, lng: lng }" />
-      </GmapMap>
-    </template>
+    <GmapMap
+      :center="{
+        lat: Number(architecture.lat),
+        lng: Number(architecture.lng)
+      }"
+      :zoom="17"
+      :options="options"
+      map-type-id="roadmap"
+      style="height: 300px"
+    >
+      <GmapMarker
+        :position="{
+          lat: Number(architecture.lat),
+          lng: Number(architecture.lng)
+        }"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
 export default {
   props: {
     architecture: {
@@ -25,27 +30,11 @@ export default {
   },
   data() {
     return {
-      loading: false,
-      lat: '',
-      lng: '',
       options: {
         mapTypeControl: false,
         streetViewControl: false
       }
     }
-  },
-  mounted() {
-    Vue.$geocoder.send(
-      { address_line_1: this.architecture.address },
-      (response) => {
-        if (response.results) {
-          this.lat = response.results[0].geometry.location.lat
-          this.lng = response.results[0].geometry.location.lng
-          console.log(response.results[0].geometry)
-          this.loading = true
-        }
-      }
-    )
   }
 }
 </script>
