3793f5004c3568b2f8ffb74b4d63a53f3234d176
yasu2450 <hiro0130.tech@gmail.com>
2022-01-13 03:35:33 +0900

    add: factorybotのデータにimageを追加してエラーの解消#279

--- back/spec/factories/posts.rb
+++ back/spec/factories/posts.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     association :user
     title { 'a' * 15 }
     content { 'a' * 100 }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample.jpg')) }
   end
 end
