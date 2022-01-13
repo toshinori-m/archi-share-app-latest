82deaac0c227ff0124d1ec3a4e5c39cab7a8d24d
yasu2450 <hiro0130.tech@gmail.com>
2022-01-14 04:16:20 +0900

    fix: rubocopの指摘箇所の修正#281

--- back/spec/requests/api/v1/relationships_spec.rb
+++ back/spec/requests/api/v1/relationships_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Relationship, type: :request do
         post '/api/v1/relationships', params: { user_id: @user.id, follow_id: @other_user.id }
         expect(response).to have_http_status(:created)
       end
-  
+
       it 'ユーザーをフォロー解除' do
         @relationship = create(:relationship)
         delete '/api/v1/relationships', params: { user_id: @relationship.user_id, follow_id: @relationship.follow_id }
