1c03be27a6b4ebcfe70adc9a3f5b965e92e64e5e
yasu2450 <hiro0130.tech@gmail.com>
2022-01-22 07:28:58 +0900

    add: PostLikeの統合テストの作成#343

--- back/spec/requests/api/v1/post_likes_spec.rb
+++ back/spec/requests/api/v1/post_likes_spec.rb
@@ -1,7 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::PostLikes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe PostLike, type: :request do
+  describe 'いいね機能' do
+    before do
+      @user = create(:user)
+      @post = create(:post)
+      @postlike = create(:post_like)
+    end
+
+    context '投稿をいいね・いいね解除できる場合' do
+      it '投稿をいいね' do
+        post '/api/v1/post_likes', params: { user_id: @user.id, post_id: @post.id }
+        expect(response).to have_http_status(:created)
+      end
+
+      it '投稿をいいね解除' do
+        delete '/api/v1/post_likes', params: { user_id: @postlike.user_id, post_id: @postlike.post_id }
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context '投稿をいいねできない場合' do
+      it 'user_idがない場合' do
+        post '/api/v1/post_likes', params: { user_id: nil, post_id: @post.id }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'post_idがない場合' do
+        post '/api/v1/post_likes', params: { user_id: @user.id, post_id: nil }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'すでにいいねしている場合' do
+        post '/api/v1/post_likes', params: { user_id: @postlike.user_id, post_id: @postlike.post_id }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context '投稿をいいね解除できない場合' do
+      it 'いいねしていない投稿のいいね解除の場合' do
+        delete '/api/v1/post_likes', params: { user_id: @user.id, post_id: @post.id }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
