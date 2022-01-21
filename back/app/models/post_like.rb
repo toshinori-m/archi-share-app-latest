d35e8374adaa2100bb3b3bd092b14ac8eb4cad46
yasu2450 <hiro0130.tech@gmail.com>
2022-01-21 23:55:08 +0900

    add: PostLikeのモデルテストの作成#341

--- back/app/models/post_like.rb
+++ back/app/models/post_like.rb
@@ -2,6 +2,6 @@ class PostLike < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :post_id }
   validates :post_id, presence: true
 end
--- back/spec/models/post_like_spec.rb
+++ back/spec/models/post_like_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe PostLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'いいね機能' do
+    before do
+      @postlike = create(:post_like)
+    end
+
+    context 'いいねできる場合' do
+      it '全ての値が正しい場合' do
+        expect(@postlike).to be_valid
+      end
+
+      it 'post_idが同じでuser_idが異なる場合' do
+        @like = create(:post_like, post_id: @postlike.post_id)
+        expect(@like).to be_valid
+      end
+
+      it 'user_idが同じでpost_idが異なる場合' do
+        @like = create(:post_like, user_id: @postlike.user_id)
+        expect(@like).to be_valid
+      end
+    end
+
+    context 'いいねできない場合' do
+      it 'user_idがない場合' do
+        @postlike.user_id = nil
+        expect(@postlike).to be_invalid
+      end
+
+      it 'post_idがない場合' do
+        @postlike.post_id = nil
+        expect(@postlike).to be_invalid
+      end
+    end
+
+    context '一意性の検証' do
+      it 'user_idとpost_idの組み合わせは一意であること' do
+        @like = build(:post_like, user_id: @postlike.user_id, post_id: @postlike.post_id)
+        expect(@like).to be_invalid
+      end
+    end
+  end
 end
