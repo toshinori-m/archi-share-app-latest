f1cb85e7551b76b12d589ca5b173086c2fc07b22
yasu2450 <hiro0130.tech@gmail.com>
2022-04-03 01:27:43 +0900

    fix: UserのRequest Specの修正#397

--- back/spec/requests/api/v1/users_spec.rb
+++ back/spec/requests/api/v1/users_spec.rb
@@ -1,22 +1,35 @@
 require 'rails_helper'
-RSpec.describe User, type: :request do
-  describe 'User' do
-    before(:each) do
-      @user = create(:user)
-    end
 
-    it 'ユーザーの一覧の取得' do
-      get '/api/v1/users'
-      expect(response.status).to eq(200)
-    end
+RSpec.describe Api::V1::UsersController, type: :request do
+  let!(:user) { create(:user) }
 
-    it '特定のユーザーの取得' do
-      get "/api/v1/users/#{@user.id}"
-      expect(json['name']).to eq(@user.name)
-    end
+  describe 'GET /api/v1/users' do
+    subject(:request) { get api_v1_users_path }
 
-    it '特定のユーザーの削除' do
-      expect { delete "/api/v1/users/#{@user.id}" }.to change { User.count }.by(-1)
+    it 'ユーザーの一覧を取得出来ること' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.size).to eq 1
+    end
+  end
+
+  describe 'GET /api/v1/users/:id' do
+    subject(:request) { get api_v1_user_path(user.id) }
+
+    it '特定のユーザーを取得出来ること' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['id']).to eq user.id
+    end
+  end
+
+  describe 'DELETE /api/v1/users/:id' do
+    subject(:request) { delete api_v1_user_path(user.id) }
+
+    it '特定のユーザーを削除出来ること' do
+      expect { request }.to change(User, :count).by(-1)
     end
   end
 end
