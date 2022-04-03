7d25fda6745240cdc17c1bbc9568d12afece4a82
yasu2450 <hiro0130.tech@gmail.com>
2022-04-04 01:08:17 +0900

    fix: PostのRequest Specの修正#399

--- back/spec/factories/posts.rb
+++ back/spec/factories/posts.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :post do
-    association :user
     title { 'a' * 15 }
     content { 'a' * 100 }
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample.jpg')) }
+
+    association :user
   end
 end
--- back/spec/requests/api/v1/posts_spec.rb
+++ back/spec/requests/api/v1/posts_spec.rb
@@ -1,56 +1,83 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :request do
-  describe 'Post' do
-    before(:each) do
-      @post = create(:post)
-    end
+RSpec.describe Api::V1::PostsController, type: :request do
+  describe 'GET /api/v1/posts' do
+    subject(:request) { get api_v1_posts_path }
+    let!(:post) { create(:post) }
 
-    it '投稿の一覧の取得' do
-      get '/api/v1/posts'
-      expect(response.status).to eq(200)
+    it '投稿の一覧を取得出来ること' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.size).to eq 1
     end
+  end
 
-    it '特定の投稿の取得' do
-      get "/api/v1/posts/#{@post.id}"
-      expect(json['title']).to eq(@post.title)
+  describe 'GET /api/v1/posts/:id' do
+    subject(:request) { get api_v1_post_path(post.id) }
+    let!(:post) { create(:post) }
+
+    it '特定の投稿を取得出来ること' do
+      request
+      expect(response).to have_http_status(:ok)
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body['id']).to eq post.id
     end
+  end
 
-    context '投稿の作成' do
-      before do
-        @user = create(:user)
+  describe 'POST /api/v1/posts' do
+    subject(:request) { post api_v1_posts_path, params: params }
+    let(:image) { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample.jpg')) }
+    let(:user) { create(:user) }
+
+    context 'paramsの値が正しい場合' do
+      let(:params) { { title: 'タイトル', content: '内容', image: image, user_id: user.id } }
+
+      it '投稿を作成出来ること' do
+        expect { request }.to change(Post, :count).by(+1)
       end
+    end
 
-      it '投稿が作成できる場合' do
-        image = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample.jpg'))
-        params = { title: 'a', content: 'a', image: image, user_id: @user.id }
-        expect { post '/api/v1/posts', params: params }.to change { Post.count }.by(+1)
-      end
+    context 'paramsの値が正しくない場合' do
+      let(:params) { { title: 'タイトル', content: '', image: image, user_id: user.id } }
 
-      it '投稿が作成できない場合' do
-        image = Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample.jpg'))
-        params = { title: 'a', content: '', image: image, user_id: @user.id }
-        post '/api/v1/posts', params: params
+      it '投稿を作成出来ないこと' do
+        request
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
+  end
 
-    context '投稿の更新' do
-      it '投稿が更新できる場合' do
-        params = { title: 'b', content: 'b', user_id: @post.user_id }
-        put "/api/v1/posts/#{@post.id}", params: params
-        expect(json['title']).to eq('b')
-      end
+  describe 'PATCH /api/v1/posts/:id' do
+    subject(:request) { patch api_v1_post_path(post.id), params: params }
+    let!(:post) { create(:post) }
 
-      it '投稿が更新できない場合' do
-        params = { title: '', content: 'b', user_id: @post.user_id }
-        put "/api/v1/posts/#{@post.id}", params: params
-        expect(response).to have_http_status(:unprocessable_entity)
+    context 'paramsの値が正しい場合' do
+      let(:params) { { title: '投稿' } }
+
+      it '投稿を更新出来ること' do
+        request
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body['title']).to eq '投稿'
       end
     end
 
-    it '特定の投稿の削除' do
-      expect { delete "/api/v1/posts/#{@post.id}" }.to change { Post.count }.by(-1)
+    context 'paramsの値が正しくない場合' do
+      let(:params) { { title: '' } }
+
+      it '投稿を更新出来ないこと' do
+        request
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/posts/:id' do
+    subject(:request) { delete api_v1_post_path(post.id) }
+    let!(:post) { create(:post) }
+
+    it '特定の投稿を削除出来ること' do
+      expect { request }.to change(Post, :count).by(-1)
     end
   end
 end
