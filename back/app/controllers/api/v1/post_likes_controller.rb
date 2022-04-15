82e03a250c883d4aaad9f4d40069d669574d8317
yasu2450 <hiro0130.tech@gmail.com>
2022-04-15 21:53:51 +0900

    fix: PostLikeのRequest Specの修正#413

--- back/app/controllers/api/v1/post_likes_controller.rb
+++ back/app/controllers/api/v1/post_likes_controller.rb
@@ -1,10 +1,11 @@
 module Api
   module V1
     class PostLikesController < ApplicationController
+      before_action :set_post
+
       def create
         @postlike = PostLike.new(like_params)
         if @postlike.save
-          @post = Post.find(params[:post_id])
           render json: @post, include: [
             :user,
             :architecture,
@@ -18,23 +19,24 @@ module Api
 
       def destroy
         @user = User.find(params[:user_id])
-        @post = Post.find(params[:post_id])
-        if @user.unlike(@post)
-          render json: @post, include: [
-            :user,
-            :architecture,
-            :like_users,
-            { comments: :user }
-          ]
-        else
-          render json: @post.errors, status: :unprocessable_entity
-        end
+        return unless @user.unlike(@post)
+
+        render json: @post, include: [
+          :user,
+          :architecture,
+          :like_users,
+          { comments: :user }
+        ]
       end
 
       private
 
       def like_params
         params.permit(:user_id, :post_id)
+      end
+
+      def set_post
+        @post = Post.find(params[:post_id])
       end
     end
   end
